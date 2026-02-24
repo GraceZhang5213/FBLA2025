546e68b940b110a58c823c22cbabd52e2c458e35
GraceZhang5213 <grace.zhang@apsbulldogs.org>
2026-02-23 19:17:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ const items = [
 ]
 
 const search = document.querySelector ('.input')
+
+searchInput.addEventListener("input", (e) => {
+  let value = e.target.value
+  if (value && value.trim().length > 0 {
+    value = value.trim().toLowerCase()
+
+    setList(items.filter(items => {
+      return items.name.includes(value)
+    }))
+  } else {
+
+  }
+}
+
+const clearButton = document.getElementById('clear')
+
+clearButton.addEventListener("click", () => {
+})
+
+function itemList(results) {
+  for (const items of results) {
+    const resultItem = document.createElement('li')
+    resultItem.classList.add('result-list')
+    const text = document.createTextNode(items.name)
+    resultItem.appendChild(text)
+    list.appendChild(resultItem)
+  }
+}
